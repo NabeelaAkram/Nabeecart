1a062d6b1ae8462fa4f32386e83562e212d98325
NabeelaAkram <163669804+NabeelaAkram@users.noreply.github.com>
2024-06-13 15:01:38 +0530

    initial code commit

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+export  default function Footer (){
+    return (
+        <footer className="py-1">
+            <p className="text-center text-dark mt-1">
+                Nabeecart - 2024, All Rights Reserved
+            </p>
+        </footer>
+    )
+}
